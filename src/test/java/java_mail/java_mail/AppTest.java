d7b7f2d85302772c1ff8ccd7f2e26bddb3c93715
Bruno Wosiak <31929337+wosiakkk@users.noreply.github.com>
2019-07-04 17:32:41 -0300

    Initial commit

---
+++ src/test/java/java_mail/java_mail/AppTest.java
@@ -0,0 +1,20 @@
+package java_mail.java_mail;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+   
+	@org.junit.Test
+	public void testeEmail() {
+		
+	}
+	
+	
+}
